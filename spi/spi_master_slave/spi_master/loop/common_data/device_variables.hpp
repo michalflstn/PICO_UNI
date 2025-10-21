ee2c95981dc7d3b683cce885fc035efded9d6a1f
michalflstn <mfelsztyn@yandex.ru>
2025-10-21 16:43:18 +0300

    Update device_variables.hpp

--- spi/spi_master_slave/spi_master/loop/common_data/device_variables.hpp
+++ spi/spi_master_slave/spi_master/loop/common_data/device_variables.hpp
@@ -43,8 +43,8 @@ control registers
 #define port_Gain_LOOP 6
 #define port_None      7 
 
-#define spi_cpol  SPI_CPOL_0    
-#define spi_cpha  SPI_CPHA_1   
+#define spi_cpol  SPI_CPOL_1    
+#define spi_cpha  SPI_CPHA_0   
 //  add 250409
 extern uint8_t device;
 extern uint8_t sensor;
