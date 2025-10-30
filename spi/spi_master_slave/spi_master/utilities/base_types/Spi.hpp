96f0aa038c8e9e6aa75ad904a745416c5847a37d
michalflstn <mfelsztyn@yandex.ru>
2025-10-30 14:04:09 +0300

    Update Spi.hpp

--- spi/spi_master_slave/spi_master/utilities/base_types/Spi.hpp
+++ spi/spi_master_slave/spi_master/utilities/base_types/Spi.hpp
@@ -3,6 +3,12 @@
 
 #include "hardware/spi.h"
 
+#define PDSPI                 PICO_DEFAULT_SPI 
+#define PDSPI_SCK_PIN         2
+#define PDSPI_TX_PIN          3
+#define PDSPI_RX_PIN          0
+#define PDSPI_CSN_PIN         1
+
 class Spi
 {
 public:
